0d393156fd7d8d174bd3d5a34c287fcbfff893a2
Alex <alex.ewers84@yahoo.co.uk>
2023-01-09 13:29:42 +0000

    Dead Branch
    Yeahh...

---
+++ ZOM B/Assets/EnemyPath.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //Cause we are using nav Mesh agent
+
+public class EnemyPath : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatisGround, whatisPlayer;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    public float walkPointRange;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    //For Following the path
+    public Transform[] target;
+    public float speed;
+
+    private int current;
+    private void Awake()
+    {
+        //player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //Check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatisPlayer);
+        print(playerInSightRange);
+
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    private void Patroling()
+    {
+        if (transform.position != target[current].position)
+        {
+            Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+            GetComponent<Rigidbody>().MovePosition(pos);
+        }
+        else current = (current + 1) % target.Length;
+    }
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+}
---
+++ ZOM B/Assets/WaypointMover.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMover : MonoBehaviour
+{
+    [SerializeField] private Waypoints waypoints; //Stores ref to the waypoints.
+    [SerializeField] private float movespeed = 5f;
+    [SerializeField] private float distanceThreshold = 0.1f;
+
+    private Transform currentWaypoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Set inital pos to 1st Waypoint
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+
+        // Set next Waypoint target
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+ 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, movespeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
+        {
+            currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        }
+    }
+}
